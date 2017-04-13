b7fb312ddf6125a61555d45d1c0dc0e09f4c69ce
ryanrosenfeld <rdr6re@virginia.edu>
2017-04-13 13:21:58 -0400

    Updating to reflect changes

--- scavenger_hunt/api/views.py
+++ scavenger_hunt/api/views.py
@@ -7,10 +7,14 @@ def api_call(request):
     response = json.dumps({'None': True})
     if request.method == 'GET':
         task_num = request.GET.get('task_num', None)
-        if task_num == 1:
+        if task_num == '1':
             activate = SiteUser.objects.filter(task_num=1, activate=True)
-            if activate is not None:
+            if len(activate) > 0:
                 response = 1
+                for user in activate:
+                    user.activate = False
+                    user.task_num += 1
+                    user.save()
             else:
                 response = 0
     return render(request, 'response.html', {'response': response})
--- scavenger_hunt/questions/views.py
+++ scavenger_hunt/questions/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .forms import *
+from django.http import HttpResponseRedirect
 
 
 def task1(request):
+    if request.user.task_num > 1:
+        url = "/task" + str(request.user.task_num) + "/"
+        return HttpResponseRedirect(url)
     correct = False
     incorrect = False
     if request.method == 'POST':
@@ -11,9 +15,33 @@ def task1(request):
             if request.POST['answer'] == '1924':
                 correct = True
                 request.user.activate = True
+                request.user.save()
             else:
                 incorrect = True
             return render(request, 'task1.html', {'form': form, 'correct': correct, 'incorrect': incorrect})
     form = Task1Form()
     request.user.task_num = 1
+    request.user.save()
     return render(request, 'task1.html', {'form': form})
+
+
+def task2(request):
+    if request.user.task_num != 2:
+        url = "/task" + str(request.user.task_num) + "/"
+        return HttpResponseRedirect(url)
+    correct = False
+    incorrect = False
+    if request.method == 'POST':
+        form = Task2Form(request.POST)
+        if form.is_valid():
+            if request.POST['answer'] == '1924':
+                correct = True
+                request.user.activate = True
+                request.user.save()
+            else:
+                incorrect = True
+            return render(request, 'task2.html', {'form': form, 'correct': correct, 'incorrect': incorrect})
+    form = Task1Form()
+    request.user.task_num = 2
+    request.user.save()
+    return render(request, 'task2.html', {'form': form})
